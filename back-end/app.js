2078bde5b4fb2b8d8423bf02f4161ae4e712a522
stratos1991 <stratos1991@yahoo.gr>
2019-03-01 14:53:32 +0200

    allowed CORS x-observatory-auth

--- back-end/app.js
+++ back-end/app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization,x-observatory-auth'
   );
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
